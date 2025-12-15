03918f156b33206771e17ca3b1b1c8ff652bb2f6
moon <shawmakesmagic@gmail.com>
2025-12-15 01:22:05 -0800

    testss

--- apps/gateway/tests/eil-transfer.spec.ts
+++ apps/gateway/tests/eil-transfer.spec.ts
@@ -12,7 +12,7 @@
 
 import { testWithSynpress } from '@synthetixio/synpress'
 import { metaMaskFixtures } from '@synthetixio/synpress/playwright'
-import { basicSetup } from '../../synpress.config'
+import { basicSetup } from '../synpress.config'
 
 const test = testWithSynpress(metaMaskFixtures(basicSetup))
 const { expect } = test
